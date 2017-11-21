72f08373507cf4be4bf96dc01b64702e2e680887
Thomas Hargreaves <thomas.hargreaves@nrgfleet.com>
2017-11-21 13:43:52 +0000

    Material Layout Stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatSidenavModule,MatIconModule,MatTabsModule, MatListModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +13,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatTabsModule,
+    MatListModule,
+    MatButtonModule
   ], 
   providers: [],
   bootstrap: [AppComponent]
